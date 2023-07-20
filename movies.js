19410f6b49efea08c201eb4a69d05528261b64e6
berluche <berlucheprunier@gmail.com>
2023-07-20 03:46:54 -0400

    tried adding spinner

--- movies.js
+++ movies.js
@@ -28,6 +28,20 @@ function toggleContrast() {
     }
 }
 
+// Function to show the loading spinner
+function showLoadingSpinner() {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  loadingSpinner.style.display = "block";
+
+  const moviesContainer = document.getElementById("movie-container");
+  moviesContainer.innerHTML = ""; 
+}
+
+// Function to hide the loading spinner
+function hideLoadingSpinner() {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  loadingSpinner.style.display = "none";
+}
 
 
 // Function to fetch movies from api
@@ -38,6 +52,10 @@ async function fetchMovies(searchTerm) {
     try {
       const response = await fetch(url);
       const data = await response.json();
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      hideLoadingSpinner();
   
       return data.Search;
     } catch (error) {
@@ -72,54 +90,21 @@ async function fetchMovies(searchTerm) {
   }
   
 // Function to search movies with search btn
-  // const searchButton = document.getElementById("search-btn");
-  // searchButton.addEventListener("click", async (event) => {
-  //   event.preventDefault();
+  const searchButton = document.getElementById("search-btn");
+  searchButton.addEventListener("click", async (event) => {
+    event.preventDefault();
   
-  //   const searchInput = document.getElementById("search-input");
-  //   const searchTerm = searchInput.value.trim();
+    const searchInput = document.getElementById("search-input");
+    const searchTerm = searchInput.value.trim();
   
-  //   if (searchTerm) {
-  //     const movies = await fetchMovies(searchTerm);
-  //     displayMovies(movies);
-  //   }
-  // });
-
-  // Function to search movies with search button
-  document.addEventListener("DOMContentLoaded", () => {
-    const searchButton = document.getElementById("search-btn");
-  
-    if (searchButton) {
-      searchButton.addEventListener("click", async (event) => {
-        event.preventDefault();
-  
-        const searchInput = document.getElementById("search-input");
-        const searchTerm = searchInput.value.trim();
-  
-        if (searchTerm) {
-          const moviesContainer = document.querySelector(".movies");
-          const loadingSpinner = document.createElement("div");
-          loadingSpinner.classList.add("movies__loading--spinner");
-  
-          moviesContainer.innerHTML = "";
-          moviesContainer.appendChild(loadingSpinner);
-  
-          try {
-            const movies = await fetchMovies(searchTerm);
-            displayMovies(movies);
-          } catch (error) {
-            console.log("Error fetching movies:", error);
-          } finally {
-            setTimeout(() => {
-              loadingSpinner.remove();
-            }, 2000); // Adjust the duration (in milliseconds) as needed
-          }
-        }
-      });
-    } else {
-      console.log("Error: searchButton element not found.");
+    if (searchTerm) {
+      showLoadingSpinner();
+      const movies = await fetchMovies(searchTerm);
+      displayMovies(movies);
     }
   });
+
+  
 
 
 
